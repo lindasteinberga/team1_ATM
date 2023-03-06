51af45f7692a56aaf631f349c9e9a22a5dd4560d
Linda S <linda.steinberga.86@gmail.com>
2023-03-06 21:17:13 +0200

    language file added

---
+++ language.py
@@ -0,0 +1,11 @@
+def get_lang(lang):
+    match lang:
+        case 0:
+            return "English"
+        case 1:
+            return "Latvian"
+        case 2:
+            return "Lithuanian"
+        case 3:
+            return "German"
+
