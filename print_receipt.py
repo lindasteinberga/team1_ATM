2505bc2e7413ef1215969f519044be7c846e057d
Linda S <linda.steinberga.86@gmail.com>
2023-03-07 15:37:12 +0200

    print_receipt file added

---
+++ print_receipt.py
@@ -0,0 +1,15 @@
+def atm_receipt(name, account_number, transaction_type, amount):
+    print("--------ATM Receipt---------------")
+    print("----------------------------------")
+    print("Customer name: ", name)
+    print("Account number: ",  account_number)
+    print("Transaction Type: ", transaction_type)
+    print("Amount: ", amount)
+    print("Thank you for using our ATM!")
+
+
+
+
+
+
+
