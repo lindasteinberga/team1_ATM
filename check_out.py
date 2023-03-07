b19d205faa6b4429410f80fec55996de52fd0f67
Linda S <linda.steinberga.86@gmail.com>
2023-03-07 19:03:17 +0200

    description about ATM system, update

--- check_out.py
+++ check_out.py
@@ -3,16 +3,17 @@
 info = "Available ATM services: "
 print(info)
 
+
 def service_choice(services):
     for service in services:
         print(service)
 
-services =("1 - Balance service", "2 - Withdraw service", "3 - Deposit service", "4 - Exit service")
-service_choice(services)
 
+services_list = ("1 - Balance service", "2 - Withdraw service", "3 - Deposit service", "4 - Exit service")
+service_choice(services_list)
 
 service_list = int(input("\nPlease, choose your service by entering number: "))
-if ( service_list == 1):
+if service_list == 1:
     print("Balance service")
 elif service_list == 2:
     print("Withdraw service")
@@ -22,9 +23,3 @@ else:
     print("\nExit service")
     print("Please, take out your credit card!!!")
     print("Thank you for using ATM !!!")
-
-
-
-
-
-
