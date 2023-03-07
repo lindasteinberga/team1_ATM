d2ec290351ee653700a74145f4c219beb4f6d290
Agnija <vjakseagnija@gmail.com>
2023-03-07 18:08:37 +0200

    check out created

---
+++ check_out.py
@@ -0,0 +1,30 @@
+"""check out from atm"""
+
+info = "Available ATM services: "
+print(info)
+
+def service_choice(services):
+    for service in services:
+        print(service)
+
+services =("1 - Balance service", "2 - Withdraw service", "3 - Deposit service", "4 - Exit service")
+service_choice(services)
+
+
+service_list = int(input("\nPlease, choose your service by entering number: "))
+if ( service_list == 1):
+    print("Balance service")
+elif service_list == 2:
+    print("Withdraw service")
+elif service_list == 3:
+    print("Deposit service")
+else:
+    print("\nExit service")
+    print("Please, take out your credit card!!!")
+    print("Thank you for using ATM !!!")
+
+
+
+
+
+
