6740d970f5b0c06d85018f52d087ee0822726a3f
Rasmanda <rasmanda12@gmail.com>
2023-03-07 18:55:02 +0200

    updated

---
+++ withdraw.py
@@ -0,0 +1,9 @@
+import test
+import test as withdraw
+print("How much money would you like to withdraw?")
+amount = int(input())
+
+print("you have withdrawed", test.withdraw (int(amount)), "Euros")
+
+
+
