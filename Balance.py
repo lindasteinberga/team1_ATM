aa2293308fc776b012e16896059208a9a5366d6e
Ignejoneigne <125908665+Ignejoneigne@users.noreply.github.com>
2023-03-08 00:26:38 +0200

    Delete Balance.py

--- Balance.py
+++
@@ -1,7 +0,0 @@
-balance = 1000.00
-
-response = input("Please select from the following options: \nWithdraw_(W) \nDeposit_(D) \nBalance_(B) \nLoan_(L) \nQuit_(Q) \n Receipt_(R) Choose one letter: ").lower()
-valid_response = ["w", "d", "b", "l", "q", "r"]
-response = response.lower()
-if response == "b":
-    (print(f"Your balance is: ", balance, "EUR"))
